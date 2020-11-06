23a10360866fd88c58c09deb37f5562d646455e6
frillic <69489498+crowe828@users.noreply.github.com>
2020-11-05 19:01:51 -0500

    built searchbar

--- employee-directory/src/Components/Nav.js
+++ employee-directory/src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import SearchBox from "./SearchBox";
+import SearchBox from "./SearchBox";
 // import "../Styles/Nav.css";
 
 // { handleSearchChange }
@@ -7,10 +7,10 @@ function Nav() {
   return (
     <nav
       className="navbar navbar-expand navbar-light bg-light"
-      style={{ display: "inline-block", textAlign: "center", width: "100%" }}
+      style={{ display: "inline-block", width: "100%" }}
     >
       <div>
-        This is a navbar
+        <SearchBox />
         {/* <SearchBox handleSearchChange={handleSearchChange} />; */}
       </div>
     </nav>
---
+++ employee-directory/src/Components/SearchBox.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function SearchBox(props) {
+  return (
+    <form className="search form-inline" style={{ display: "inline-flex" }}>
+      <div className="form-group">
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="employee"
+          list="employees"
+          type="text"
+          className="form-control"
+          placeholder="Search for an employee"
+          id="employee"
+          style={{ width: "500px" }}
+        />
+        {/* <datalist id="employees">
+          {props.employees.map((employee) => (
+            <option value={employee} key={employee} />
+          ))}
+        </datalist> */}
+        <button
+          type="submit"
+          onClick={props.handleFormSubmit}
+          className="btn btn-primary"
+          style={{ marginLeft: "10px" }}
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default SearchBox;
