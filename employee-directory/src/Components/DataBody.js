966506ab9ce6358e95e058549701d0b0c7ce90a2
frillic <69489498+crowe828@users.noreply.github.com>
2020-11-06 22:48:52 -0500

    working on mapping api

--- employee-directory/src/Components/DataBody.js
+++ employee-directory/src/Components/DataBody.js
@@ -1,102 +1,32 @@
-import React, { Component } from "react";
-import API from "../Utils/API";
+import React from "react";
 
-class DataBody extends Component {
-  state = {
-    image: "",
-    name: "",
-    phone: "",
-    email: "",
-    dob: "",
-  };
-
-  componentDidMount() {
-    this.userImage();
-    this.userName();
-    this.userPhone();
-    this.userEmail();
-    this.userDob();
-  }
-
-  userImage = () => {
-    API.getUsers()
-      .then((res) =>
-        this.setState({
-          image: res.data.results[0].picture.thumbnail,
-        })
-      )
-      .catch((err) => console.log(err));
-  };
-
-  userName = () => {
-    API.getUsers()
-      .then((res) =>
-        this.setState({
-          name:
-            res.data.results[0].name.first +
-            " " +
-            res.data.results[0].name.last,
-        })
-      )
-      .catch((err) => console.log(err));
-  };
-
-  userPhone = () => {
-    API.getUsers()
-      .then((res) =>
-        this.setState({
-          phone: res.data.results[0].phone,
-        })
-      )
-      .catch((err) => console.log(err));
-  };
-
-  userEmail = () => {
-    API.getUsers()
-      .then((res) =>
-        this.setState({
-          email: res.data.results[0].email,
-        })
-      )
-      .catch((err) => console.log(err));
-  };
-
-  userDob = () => {
-    API.getUsers()
-      .then((res) =>
-        this.setState({
-          dob: res.data.results[0].dob.date,
-        })
-      )
-      .catch((err) => console.log(err));
-  };
-
-  render() {
-    return (
-      <table className="table table-hover table-dark">
-        <thead>
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col">Name</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">
-              <img src={this.state.image} alt={this.state.name} />
-            </th>
-            <td>{this.state.name}</td>
-            <td>{this.state.phone}</td>
-            <td>{this.state.email}</td>
-            <td>{this.state.dob}</td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+function DataBody(data) {
+  return (
+    <tbody>
+      {data.results.map((result) => (
+        <tr>
+          <th scope="row" key={result.id.value}>
+            <img
+              alt={
+                result.data.results.name.first +
+                " " +
+                result.data.results.name.last
+              }
+              src={result.data.results.picture.thumbnail}
+            />
+          </th>
+          <td>
+            {result.data.results.name.first +
+              " " +
+              result.data.results.name.last}
+          </td>
+          <td>{result.data.results.phone}</td>
+          <td>{result.data.results.email}</td>
+          <td>{result.data.results.dob.date}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
 }
 
 export default DataBody;
--- employee-directory/src/Components/DataTable.js
+++ employee-directory/src/Components/DataTable.js
@@ -1,8 +1,112 @@
-import React from "react";
+import React, { Component } from "react";
 import DataBody from "./DataBody";
+import API from "../Utils/API";
 
-function DataTable() {
-  return <DataBody />;
+class DataTable extends Component {
+  state = {
+    results: [],
+    image: [],
+    name: [],
+    phone: [],
+    email: [],
+    dob: [],
+  };
+
+  componentDidMount() {
+    this.userInfo();
+    this.userImage();
+    this.userName();
+    this.userPhone();
+    this.userEmail();
+    this.userDob();
+  }
+
+  userInfo = () => {
+    API.getUsers()
+      .then((res) => this.setState({ results: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+  userImage = () => {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          image: res.data.results[0].picture.thumbnail,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  userName = () => {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          name:
+            res.data.results[0].name.first +
+            " " +
+            res.data.results[0].name.last,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  userPhone = () => {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          phone: res.data.results[0].phone,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  userEmail = () => {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          email: res.data.results[0].email,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  userDob = () => {
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          dob: res.data.results[0].dob.date,
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <DataBody results={this.state.results} /> */}
+          <tr>
+            <th scope="row">
+              <img src={this.state.image} alt={this.state.name} />
+            </th>
+            <td>{this.state.name}</td>
+            <td>{this.state.phone}</td>
+            <td>{this.state.email}</td>
+            <td>{this.state.dob}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default DataTable;
